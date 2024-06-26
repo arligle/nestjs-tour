39238f3c0cbec1b632134b157869649ac5c5c6b2
7wanaio <0774543002@163.com>
2024-06-26 19:26:16 +0800

    first commit

---
+++ src/demo.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { Module, Controller, Get } from '@nestjs/common';
+
+
+@Controller()
+class AppController {
+  @Get('/nest')
+  getRootRoute() {
+    return 'hi! I am a single file nest-app';
+  }
+
+  @Get('/bye')
+  getByeThere() {
+    return 'bye! nest-app';
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule { }
+
+
+
+async function bootstrap() {
+
+  // 创建一个nestjs实例，本质上它是一个http服务器+nestjs容器
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+bootstrap();
